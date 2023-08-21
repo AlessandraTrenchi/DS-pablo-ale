bdb3fd0f5f0c104429348eaea4593828245dfb46
AlessandraTrenchi <alessandra.trenchi@studio.unibo.it>
2023-08-21 11:55:04 +0200

    base

--- scripts/data_processor.py
+++ scripts/data_processor.py
@@ -1,1 +1,76 @@
 #  define the classes responsible for handling data and querying
+
+# Base class for common methods to manage database connections
+class DataManager: 
+    def __init__(self):
+        self.dbPath = ""
+        self.endpointUrl = ""
+
+    def setDbPath(self, dbPath): # it has attribute dbPath #method setDbPath to set the value of the attribute
+        self.dbPath = dbPath
+
+    def getDbPath(self): # method used to retrieve the DbPath
+        return self.dbPath
+
+    def setEndpointUrl(self, endpointUrl): # attribute endpointUrl
+        self.endpointUrl = endpointUrl
+
+    def getEndpointUrl(self):
+        return self.endpointUrl
+
+# Class for handling relational data
+class RelationalDataProcessor(DataManager): # this class inherits the attributes and methods defined in the base class
+    #manages interactions with a relational database
+    def uploadData(self, filePath):
+        # Implement data upload logic for relational database
+        print(f"Uploading data from {filePath} to relational database") 
+        # the f before the string indicates an f-string allowig to embed the values of variables directly into the string
+        #call this method and provide a filePath to display a message indicating that data is being uploaded from that specific file to the relational database
+
+# Class for handling triplestore data
+class TriplestoreDataProcessor(DataManager):
+    def uploadData(self, filePath):
+        # Implement data upload logic for triplestore
+        print(f"Uploading data from {filePath} to triplestore")
+
+# Class for querying relational data
+class RelationalQueryProcessor:
+    def getPublicationsPublishedInYear(self, year):
+        # Implement query logic for getting publications by year from relational database
+        print(f"Getting publications published in year {year}")
+
+    def getPublicationsByAuthorId(self, authorId):
+        # Implement query logic for getting publications by author id from relational database
+        print(f"Getting publications by author id {authorId}")
+
+    # ... implement other query methods ...
+
+# Class for querying triplestore data
+class TriplestoreQueryProcessor:
+    def getMostCitedPublication(self):
+        # Implement query logic for getting most cited publication from triplestore
+        print("Getting most cited publication")
+
+    def getMostCitedVenue(self):
+        # Implement query logic for getting most cited venue from triplestore
+        print("Getting most cited venue")
+
+    # ... implement other query methods ...
+
+# Usage
+relational_data_processor = RelationalDataProcessor()
+relational_data_processor.setDbPath("path/to/relational.db")
+relational_data_processor.uploadData("data/relational_data.csv")
+
+triplestore_data_processor = TriplestoreDataProcessor()
+triplestore_data_processor.setEndpointUrl("http://example.com/sparql")
+triplestore_data_processor.uploadData("data/triplestore_data.json")
+
+relational_query_processor = RelationalQueryProcessor()
+relational_query_processor.getPublicationsPublishedInYear(2020)
+relational_query_processor.getPublicationsByAuthorId("0000-0001-9857-1511")
+
+triplestore_query_processor = TriplestoreQueryProcessor()
+triplestore_query_processor.getMostCitedPublication()
+triplestore_query_processor.getMostCitedVenue()
+
