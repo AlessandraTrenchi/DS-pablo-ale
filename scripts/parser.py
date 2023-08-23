911e2c826977d619cd4f87b0dc1fcb36f309d3fe
AlessandraTrenchi <alessandra.trenchi@studio.unibo.it>
2023-08-23 17:37:35 +0200

    sql parser

--- scripts/parser.py
+++ scripts/parser.py
@@ -1,19 +1,25 @@
 # script to load and parse RDF data using the rdflib library.
 # Import the rdflib library
-from rdflib import Graph
+import sqlparse
 
-def parse_rdf_file(rdf_file_path):
-    # Load RDF data from a file in Turtle format
-    g = Graph()
-    g.parse(rdf_file_path, format='turtle')
+def parse_sql_file(file_path):
+    with open(file_path, 'r') as sql_file:
+        sql_content = sql_file.read()
+        
+        # Tokenize the SQL content
+        statements = sqlparse.split(sql_content)
+        
+        # Process each SQL statement
+        for statement in statements:
+            parsed = sqlparse.parse(statement)
+            
+            # Process the parsed tokens (columns, tables, constraints, etc.)
+            for token in parsed[0]:
+                # Your processing logic here
+                print(token)
 
-    # Iterate over the parsed RDF triples
-    for s, p, o in g:
-        # s: subject, p: predicate, o: object
-        print(f"Subject: {s}, Predicate: {p}, Object: {o}")
+if __name__ == "__main__":
+    sql_file_path = "database.sql"
+    parse_sql_file(sql_file_path)
 
-if __name__ == "__main__": #run the code inside when executing the script directly
-    rdf_file_path = 'data.rdf' #remember to have data.rdf file is in the same directory
-    parse_rdf_file(rdf_file_path)
-
-#if you run python rdf_parser.py in the command line, it will parse the RDF data in data.rdf and print the triples
+#if you run python parser.py in the command line, it will parse the RDF data in data.rdf and print the triples
