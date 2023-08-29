867275c393bc72bed06cf95a8d124a18605197e4
AlessandraTrenchi <alessandra.trenchi@studio.unibo.it>
2023-08-29 13:00:02 +0200

    populate update

--- scripts/populate.py
+++ scripts/populate.py
@@ -1,32 +1,46 @@
-import csv #python libraries
+import sqlite3 # for working with the sqlite database
+import csv
 import json
-import sqlite3 #to interact with the SQLite database
+
+
+def insert_publisher(cursor, id, name): 
+    query = "INSERT INTO Publishers (id, name) VALUES (?, ?)" #query to insert data into the Publishers table
+    cursor.execute(query, (id, name)) #execute the query with the provided data
+
+def insert_publication(cursor, id, doi):
+    query = "INSERT INTO Publications (id, doi) VALUES (?, ?)"
+    cursor.execute(query, (id, doi))
+
+def insert_reference(cursor, source_doi, target_doi):
+    query = "INSERT INTO References (source_doi, target_doi) VALUES (?, ?)"
+    cursor.execute(query, (source_doi, target_doi))
 
 # Open the database connection
-conn = sqlite3.connect('database.db')
-cursor = conn.cursor() # create a cursor object
+conn = sqlite3.connect('ds-pablo-ale.db')
+cursor = conn.cursor() #creating a cursor object
 
-def parse_line(line):
-    return line.strip().split(',')
-# for CSV data use the split() method to split the line into individual fields
+# Populate Publishers from CSV
+with open('publishers.csv', 'r') as csv_file: # publishers.csv', creates a CSV reader, and iterates over each row in the CSV file
+    csv_reader = csv.reader(csv_file) #  skips the header row using next(csv_reader)
+    next(csv_reader)  # Skip header row
 
-def insert_data_into_database(connection, data):
-    query = f"INSERT INTO table_name (column1, column2, column3) VALUES (?, ?, ?)"
-    connection.execute(query, data)
+    for row in csv_reader:
+        insert_publisher(cursor, *row)
 
+# Populate Publications from JSON
+with open('publications.json', 'r') as json_file:
+    publications_data = json.load(json_file)
 
-def open_database_connection(db_path):
-    try:
-        conn = sqlite3.connect(db_path)
-        print("Database connection opened")
-        return conn
-    except sqlite3.Error as e:
-        print(f"Error opening database connection: {e}")
-        return None
+    for publication in publications_data:
+        insert_publication(cursor, publication['id'], publication['doi'])
 
-def insert_data_into_database(connection, data):
-    query = f"INSERT INTO table_name (column1, column2, column3) VALUES (?, ?, ?)"
-    connection.execute(query, data)
+# Populate References from JSON
+with open('references.json', 'r') as json_file:
+    references_data = json.load(json_file)
 
-def close_database_connection(connection): #closing the database connection when you're done using 
-    connection.close()
+    for reference in references_data:
+        insert_reference(cursor, reference['source_doi'], reference['target_doi'])
+
+# Commit the changes and close the connection
+conn.commit()
+conn.close()
