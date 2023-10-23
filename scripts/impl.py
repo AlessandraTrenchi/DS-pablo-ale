e0ad94072a9477c4ed7013eb9b79199eebefd64f
AlessandraTrenchi <alessandra.trenchi@studio.unibo.it>
2023-10-23 22:18:31 +0200

    parsed json file

--- scripts/impl.py
+++ scripts/impl.py
@@ -1,16 +1,41 @@
-class RelationalProcessor:
-    def __init__(self):
+import os
+
+class RelationalDataProcessor:
+    def __init__(self): #class constructor, initializes the class instance
         self.db_path = None
 
-    def setDbPath(self, db_path):
+    def uploadData(self, path):
         """
-        Set the path to the relational database.
+        Upload data to the relational database.
 
         Args:
-            db_path (str): The path to the relational database.
-        """
-        self.db_path = db_path
+            path (str): The path to the data file to be uploaded.
 
+        Returns:
+            bool: True if the data was successfully uploaded, False otherwise.
+        """
+        if not self.db_path:
+            print("Database path is not set. Please set the database path first.")
+            return False
+
+        if not os.path.exists(path):
+            print(f"File not found: {path}")
+            return False
+
+        # Here, you can add the logic to upload the data to the database.
+        # You'll need to implement the database-specific code here.
+        # Return True if the data was successfully uploaded, or False otherwise.
+
+        # Example code (SQLite):
+        try:
+            # Your data upload logic here
+            print(f"Data uploaded from {path} to {self.db_path}")
+            return True
+        except Exception as e:
+            print(f"Error uploading data: {str(e)}")
+            return False
+
+class RelationalProcessor(RelationalDataProcessor):
     def getDbPath(self):
         """
         Get the path to the relational database.
@@ -20,9 +45,40 @@ class RelationalProcessor:
         """
         return self.db_path
 
+    def setDbPath(self, db_path):
+        """
+        Set the path to the relational database.
+
+        Args:
+            db_path (str): The path to the relational database.
+
+        Returns:
+            bool: True if the database path was successfully set, False otherwise.
+        """
+        if os.path.exists(db_path):
+            self.db_path = db_path
+            return True
+        else:
+            print(f"Database path '{db_path}' does not exist.")
+            return False
+
 # Example usage:
 if __name__ == "__main__":
     rp = RelationalProcessor()
-    rp.setDbPath("/data/database.sql") #is this the correct database name? or does it need to have .db as extension
+    
+    # Setting the database path
+    success = rp.setDbPath("./pabloale.db")
+    if success:
+        print("Database path set successfully.")
+    
+    # Uploading data
+    data_path = "/data/data_to_upload.txt"  # Path to the data file
+    upload_success = rp.uploadData(data_path)
+    if upload_success:
+        print("Data upload successful")
+    else:
+        print("Data upload failed")
+
+    # Retrieving the database path
     db_path = rp.getDbPath()
     print("Database path:", db_path)
--- scripts/parser.py
+++ scripts/parser.py
@@ -1,68 +1,94 @@
 import pandas as pd
 import json
 
-# Function to parse CSV data
-def parse_csv(csv_file):
-    df = pd.read_csv(csv_file)
-    print("Processing CSV Data:")
-    for index, row in df.iterrows():
-        print(f'CSV Row {index + 1}:')
-        for column, value in row.items():
-            print(f'{column}: {value}')
-
-# Function to parse JSON data
+# Function to parse JSON data and extract authors, venues, references, and publishers
 def parse_json(json_file):
-    extracted_data = {}  # Dictionary to store extracted data
+    extracted_data = {"Authors": [], "Venues": [], "References": [], "Publishers": []}  # Initialize lists to store extracted data
+
     with open(json_file, 'r') as json_data:
         data = json.load(json_data)
-    
+
     print("Processing JSON Data:")
-    
-    # Handle "authors" section
+
+    # Handle the "authors" section if it exists
     if "authors" in data:
-        extracted_data["authors"] = {}
-        for doi, authors_list in data["authors"].items():
-            extracted_data["authors"][doi] = []
+        authors_data = data["authors"]
+        for doi, authors_list in authors_data.items():
             for author_info in authors_list:
                 if isinstance(author_info, dict):
-                    extracted_data["authors"][doi].append(author_info)
-    
-    # Handle "venues_id" section
+                    extracted_author = {
+                        "DOI": doi,
+                        "family": author_info.get("family", ""),
+                        "given": author_info.get("given", ""),
+                        "orcid": author_info.get("orcid", ""),
+                    }
+                    extracted_data["Authors"].append(extracted_author)
+
+    # Handle the "venues_id" section if it exists
     if "venues_id" in data:
-        extracted_data["venues_id"] = {}
-        for doi, venues_list in data["venues_id"].items():
-            extracted_data["venues_id"][doi] = venues_list
-    
-    # Handle "publishers" section
-    if "publishers" in data:
-        extracted_data["publishers"] = {}
-        for publisher_id, publisher_info in data["publishers"].items():
-            extracted_data["publishers"][publisher_id] = publisher_info
-    
-    # Handle "references" section
+        venues_data = data["venues_id"]
+        for doi, identifiers_list in venues_data.items():
+            for identifier in identifiers_list:
+                if isinstance(identifier, dict):
+                    # Depending on the actual structure of the "venues_id" data, extract "issn" and "isbn"
+                    issn = identifier.get("issn", "")
+                    isbn = identifier.get("isbn", "")
+
+                    extracted_venue = {
+                        "DOI": doi,
+                        "issn": issn,
+                        "isbn": isbn,
+                    }
+                    extracted_data["Venues"].append(extracted_venue)
+
+    # Handle the "references" section if it exists
     if "references" in data:
-        extracted_data["references"] = {}
-        for doi, references_list in data["references"].items():
-            extracted_data["references"][doi] = references_list
-    
-    return extracted_data  # Return the extracted data as a dictionary
+        references_data = data["references"]
+        for doi, references_list in references_data.items():
+            for reference_info in references_list:
+                if isinstance(reference_info, dict):
+                    extracted_reference = {
+                        "DOI": doi,
+                        "reference_DOI": reference_info.get("reference_DOI", ""),
+                        "other_data": reference_info.get("other_data", []),
+                    }
+                    extracted_data["References"].append(extracted_reference)
 
-# Example usage of the function
-if __name__ == "__main__":
-    json_file = 'your_json_file.json'  # Replace with your JSON file's path
-    extracted_data = parse_json(json_file)
+    # Handle the "publishers" section if it exists
+    if "publishers" in data:
+        publishers_data = data["publishers"]
+        for publisher_id, publisher_info in publishers_data.items():
+            if "crossref" in publisher_info:
+                crossref_data = publisher_info["crossref"]
+                extracted_publisher = {
+                    "publisher_id": publisher_id,
+                    "name": crossref_data.get("name", ""),
+                }
+                extracted_data["Publishers"].append(extracted_publisher)
 
+    return extracted_data
 
 # Example usage of the functions
 if __name__ == "__main__":
-    csv_file = 'data/relational_publications.csv'
-    json_file = 'data/relational_other_data.json'
+    json_file = 'data/relational_other_data.json'  # Replace with your JSON file's path
 
-    # Parse CSV data
-    parse_csv(csv_file)
-
-    # Parse JSON data
+    # Parse JSON data and extract authors, venues, references, and publishers
     extracted_data = parse_json(json_file)
 
-    # Now, extracted_data contains a dictionary with nested dictionaries extracted from the JSON file
+    # Now, extracted_data contains lists of author, venue, reference, and publisher dictionaries, ready for insertion into the respective tables.
     # You can further process, map, or save this data as needed.
+    print("Extracted Authors Data:")
+    for author in extracted_data["Authors"]:
+        print(author)
+
+    print("Extracted Venues Data:")
+    for venue in extracted_data["Venues"]:
+        print(venue)
+
+    print("Extracted References Data:")
+    for reference in extracted_data["References"]:
+        print(reference)
+
+    print("Extracted Publishers Data:")
+    for publisher in extracted_data["Publishers"]:
+        print(publisher)
